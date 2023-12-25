d560da3c916470df697a5c25abba4182785ee95d
TaeWooKim-SCH <zop1234@hanmail.net>
2023-12-25 17:20:32 +0900

    feat: 사이드바 구현 완료

--- src/app/_components/Layouts/HeaderNav.tsx
+++ src/app/_components/Layouts/HeaderNav.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 import Link from "next/link";
-import { IoMenu, IoClose } from "react-icons/io5";
+import SideBar from "../Sidebar";
 
 export default function HeaderNav() {
   const headerItems = ["모든 동아리", "공연예술", "종교", "봉사", "교양학술", "체육", "전시창작", "준동아리"];
@@ -31,19 +31,7 @@ export default function HeaderNav() {
             href="/signup"
           >회원가입</Link>
         </div>
-        <div className="ml-2 sm:ml-5 xl:hidden">
-          <IoMenu className="sm:size-[40px]" size="30" color="#26539C" />
-        </div>
-        <div className="absolute top-0 right-[-300px] h-full bg-blue">
-          <div className="text-end mt-3 mr-3 mb-5">
-            <IoClose className="inline-block sm:size-[40px]" fill="white" size="30" />
-          </div>
-          <div className="px-20 flex flex-col items-center text-white sm:text-xl">
-            {headerItems.map((item) => (
-              <Link className="mb-5" href={`/clubs?name=${item}`} key={item}>{item}</Link>
-            ))}
-          </div>
-        </div>
+        <SideBar />
       </section>
     </header>
   );
