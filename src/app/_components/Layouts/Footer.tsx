c8c97d457e17e786fab160fe9a8eb6d62471a0f1
TaeWooKim-SCH <zop1234@hanmail.net>
2023-12-28 15:07:23 +0900

    feat: footer 반응형

--- src/app/_components/Layouts/Footer.tsx
+++ src/app/_components/Layouts/Footer.tsx
@@ -2,11 +2,11 @@ import Image from "next/image";
 
 export default function Footer() {
   return (
-    <footer className="w-full h-[100px] bg-light-blue text-white text-sm flex justify-evenly items-center">
+    <footer className="w-full h-[100px] bg-light-blue flex justify-evenly items-center">
       <section>
-        <Image className="w-[100px]" src="/Logo.svg" alt="로고" width={400} height={0} />
+        <Image className="w-[80px] sm:w-[100px]" src="/Logo.svg" alt="로고" width={400} height={0} />
       </section>
-      <section className="text-end flex flex-col justify-center">
+      <section className="text-end text-white text-[0.6rem] sm:text-sm flex flex-col justify-center">
         <div className="mb-1">contact: zop1234@hanmail.net</div>
         <div>© 2024 soondong Powered by Next.js, Vercel App</div>
       </section>
