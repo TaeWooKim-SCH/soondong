59788d94d8b9db6997924943def14941a2a0bf8c
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-04 18:09:06 +0900

    feat: 로그인 상태 관리 완료

--- src/app/_components/LoginSection.tsx
+++ src/app/_components/LoginSection.tsx
@@ -1,32 +1,34 @@
 'use client'
 
+import { signOut, useSession } from "next-auth/react";
 import Link from "next/link";
-import { useState } from "react";
 
 export default function LoginSection() {
-  const [isLogin, setIsLogin] = useState(() => {
-    if (typeof(window) !== 'undefined' ? sessionStorage.getItem('loginInfo') : null) return true;
-    else return false;
-  }); // 로그인 상태
+  const { data: session, status } = useSession();
 
-  return (
-    <>
-      {isLogin ? (
-        <div>
-        로그인상태
-        </div>
-      ) : (
-        <div className="flex items-center">
-          <Link
-            className="text-xs py-1 px-4 border border-blue rounded-md mr-2 sm:text-sm sm:py-2 sm:px-7 sm:mr-5"
-            href="/login"
-          >로그인</Link>
-          <Link
-            className="text-xs py-1 px-4 bg-blue text-white rounded-md sm:text-sm sm:py-2 sm:px-7"
-            href="/signup"
-          >회원가입</Link>
-        </div>
-      )}
-    </>
-  );
+  if (session) {
+    return (
+      <div>
+        <button
+          className="text-xs py-1 px-4 border border-blue rounded-md mr-2 sm:text-sm sm:py-2 sm:px-7 sm:mr-5"
+          onClick={() => signOut()}
+        >로그아웃</button>
+      </div>
+    );
+  }
+
+  else {
+    return (
+      <div className="flex items-center">
+        <Link
+          className="text-xs py-1 px-4 border border-blue rounded-md mr-2 sm:text-sm sm:py-2 sm:px-7 sm:mr-5"
+          href="/login"
+        >로그인</Link>
+        <Link
+          className="text-xs py-1 px-4 bg-blue text-white rounded-md sm:text-sm sm:py-2 sm:px-7"
+          href="/signup"
+        >회원가입</Link>
+      </div>
+    );
+  }
 }
