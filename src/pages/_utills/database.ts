d48e072812e643e5cb7d4997d39fb69f44d41fb1
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-10 13:09:37 +0900

    fix: 버그 수정

--- src/pages/_utills/database.ts
+++ src/pages/_utills/database.ts
@@ -4,7 +4,7 @@ let db: Connection;
 
 const connectionInfo = {
   host: process.env.MYSQL_HOST,
-  port: Number(process.env.MYSQL_PORT),
+  port: Number(process.env.MYSQL_PORT) || 3306,
   user: process.env.MYSQL_USER,
   password: process.env.MYSQL_PASSWORD,
   database: process.env.MYSQL_DATABASE
