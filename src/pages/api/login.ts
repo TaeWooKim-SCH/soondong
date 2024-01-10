e0c9cf3007e856cb443ce272a95571b596d1e5f5
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-10 12:52:59 +0900

    fix: 버그 수정

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -2,14 +2,14 @@ import { NextApiRequest, NextApiResponse } from "next";
 import { db } from "../_utills/database";
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  // db.query('select * from tb_member', (err, result) => {
-  //   if (err) {
-  //     console.error(err);
-  //   }
-  //   else {
-  //     return res.json(result);
-  //   }
-  // });
+  db.query('select * from tb_member', (err, result) => {
+    if (err) {
+      console.error(err);
+    }
+    else {
+      return res.json(result);
+    }
+  });
 
-  // db.end();
+  db.end();
 }
