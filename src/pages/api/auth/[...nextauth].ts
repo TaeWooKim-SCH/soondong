adb355776e04390b9464b83a9539a546c6c3e341
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-11 14:24:34 +0900

    feat: 로그인에서 회원가입 이동 처리중

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -19,7 +19,7 @@ export const authOptions: NextAuthOptions = {
       return token;
     },
     async redirect({ url, baseUrl }) {
-      return `${baseUrl}/home`;
+      return `${baseUrl}/signup/form`;
     }
   }
 };
