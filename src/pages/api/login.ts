4b8c52bb1fc6427fc46909208521696049925aea
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-15 18:26:55 +0900

    feat: auth 권한 정보에 따라 코드 수정중

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -6,17 +6,20 @@ const secret = process.env.NEXT_AUTH_SECRET;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const token = await getToken({ req, secret });
+  console.log(token);
   if (token) {
     db.execute(`select name from tb_member where name = '${token?.name}';`, (err, result) => {
       if (err) {
         console.error(err);
       }
       else {
-        console.log(result);
         if (Array.isArray(result) && !result.length) return res.redirect('/signup/form');
         else return res.redirect('/home');
       }
     });
+  }
+  else {
+    return res.redirect('/home').status(404);
   }
 
   db.end();
