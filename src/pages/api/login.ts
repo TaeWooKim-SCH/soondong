6d1404b32903a4232731e12c9396ce97e9163c4c
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-16 15:51:59 +0900

    fix: 로그인 api 경로에서 요청별 switch문으로 구분

--- src/pages/api/login.ts
+++ src/pages/api/login.ts
@@ -5,22 +5,39 @@ import { getToken } from "next-auth/jwt";
 const secret = process.env.NEXT_AUTH_SECRET;
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
-  const token = await getToken({ req, secret });
-  console.log(token);
-  if (token) {
-    db.execute(`select name from tb_member where name = '${token?.name}';`, (err, result) => {
-      if (err) {
-        console.error(err);
+  switch (req.method) {
+    case 'GET':
+      const token = await getToken({ req, secret });
+      
+      if (token) {
+        db.query(`select name from tb_member where name = '${token?.name}';`, (err, result) => {
+          if (err) {
+            console.error(err);
+            db.end();
+            return res.status(500).send("내부 서버 오류");
+          }
+          else {
+            if (Array.isArray(result) && !result.length) {
+              db.end();
+              return res.redirect('/signup/form');
+            }
+            else {
+              db.end();
+              return res.redirect('/home');
+            }
+          }
+        });
       }
       else {
-        if (Array.isArray(result) && !result.length) return res.redirect('/signup/form');
-        else return res.redirect('/home');
+        return res.redirect('/home').status(404);
       }
-    });
-  }
-  else {
-    return res.redirect('/home').status(404);
-  }
 
-  db.end();
+      break;
+    case 'POST':
+      console.log('post 요청');
+      break;
+    case 'DELETE':
+      console.log('DELETE 요청');
+      break;
+  }
 }
