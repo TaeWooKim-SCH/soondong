41ee548b9b262d3012c539714bbcff2fb6a8eb31
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-19 15:12:51 +0900

    feat: react-hook-form 적용

--- src/app/_components/SelectCollege.tsx
+++ src/app/_components/SelectCollege.tsx
@@ -1,10 +1,13 @@
+import { collegeInfo } from "../_modules/data";
+
 export default function SelectColleage() {
   return (
     <section className="grid grid-cols-2 gap-5">
       <select className="px-1 py-2 mb-5 outline-none bg-bg-color border-b border-b-silver">
         <option>단과대 선택</option>
-        <option>SW중심대학</option>
-        <option>공과대학</option>
+        {collegeInfo.map((college, idx) => (
+          <option key={idx}>{college.collegeName}</option>
+        ))}
       </select>
       <select className="px-1 py-2 mb-5 outline-none bg-bg-color border-b border-b-silver">
         <option>학과 선택</option>
--- src/app/_components/SignupForm.tsx
+++ src/app/_components/SignupForm.tsx
@@ -1,9 +1,14 @@
 'use client'
 
+import { SubmitHandler, useForm } from "react-hook-form";
 import SelectColleage from "./SelectCollege";
 import SignupInput from "./SignupInput";
 
 export default function SignupForm() {
+  const { register, handleSubmit, watch, formState } = useForm<FormInputs>();
+
+  const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
+
   const emailAuthHandler = async (email: string) => {
     const res = await fetch('api/auth/email', {
       method: 'POST',
@@ -15,24 +20,31 @@ export default function SignupForm() {
   }
 
   return (
-    <form className="grid grid-cols-1">
+    <form className="grid grid-cols-1" onSubmit={handleSubmit(onSubmit)}>
       <section>
-        <SignupInput placeholder="아이디" />
+        <SignupInput placeholder="아이디" register={{ ...register('id') }} />
         <button type="button" className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1">중복확인</button>
       </section>
-      <SignupInput placeholder="비밀번호" />
-      <SignupInput placeholder="비밀번호 확인" />
-      <SignupInput placeholder="이름 (닉네임X)" />
-      <SignupInput placeholder="학번" />
+      <SignupInput placeholder="비밀번호" register={{ ...register('password') }} />
+      <SignupInput placeholder="비밀번호 확인" register={{ ...register('password_confirm') }} />
+      <SignupInput placeholder="이름 (닉네임X)" register={{ ...register('name') }} />
+      <SignupInput placeholder="학번" register={{ ...register('student_id') }} />
       <SelectColleage />
-      <SignupInput placeholder="전화번호 ('-'를 빼고 입력해주세요)" />
+      <SignupInput placeholder="전화번호 ('-'를 빼고 입력해주세요)" register={{ ...register('phone_number') }} />
       <section>
-        <SignupInput placeholder="학교 이메일"/>
-        <button type="button" className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1">이메일 인증</button>
+        <SignupInput placeholder="학교 이메일" register={{ ...register('school_email') }} />
+        <button
+          className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1"
+          type="button"
+          {...register('school_auth', { value: false })}
+        >이메일 인증</button>
       </section>
       <section>
-        <SignupInput placeholder="인증번호"/>
-        <button type="button" className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1">확인</button>
+        <SignupInput placeholder="인증번호" register={{ ...register('school_auth_code') }} />
+        <button
+          className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1"
+          type="button"
+        >확인</button>
       </section>
       <section className="mt-10">
         <div className="font-bold mb-1">약관동의</div>
@@ -55,4 +67,18 @@ export default function SignupForm() {
       <button className="mt-10 bg-blue text-white px-5 py-2 rounded-md font-normal">회원가입</button>
     </form>
   );
+}
+
+interface FormInputs {
+  id: string;
+  password: string;
+  password_confirm: string;
+  name: string;
+  student_id: string;
+  school_college: string;
+  school_department: string;
+  phone_number: string;
+  school_email: string;
+  school_auth: boolean;
+  school_auth_code: string;
 }
