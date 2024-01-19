4b96f66d6816f4c82f26e94d4287583207744102
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-20 00:35:13 +0900

    feat: 이메일 인증 구현 완료

--- src/app/_components/SignupForm.tsx
+++ src/app/_components/SignupForm.tsx
@@ -1,15 +1,20 @@
 'use client'
 
 import { SubmitHandler, useForm } from "react-hook-form";
+import cryptoJS from 'crypto-js';
+
 import SelectColleage from "./SelectCollege";
 import SignupInput from "./SignupInput";
 
 export default function SignupForm() {
-  const { register, handleSubmit, watch, formState } = useForm<FormInputs>();
+  const { register, handleSubmit, watch, setValue, formState } = useForm<FormInputs>();
 
   const onSubmit: SubmitHandler<FormInputs> = (data) => console.log(data);
 
   const emailAuthHandler = async (email: string) => {
+    // TODO: 쓰로틀 또는 디바운스 등을 이용해 요청을 제한해야 함
+    alert(`${email}로 인증번호를 전송했습니다. 메일함을 확인해주세요.`);
+
     const res = await fetch('api/auth/email', {
       method: 'POST',
       headers: {
@@ -17,6 +22,26 @@ export default function SignupForm() {
       },
       body: JSON.stringify({ email })
     });
+
+    const json: EmailAuthRes = await res.json();
+    sessionStorage.setItem('email_auth_code', json.auth_code);
+  }
+
+  const emailAuthConfirmHandler = (code: string) => {
+    const encryptedCode = sessionStorage.getItem('email_auth_code');
+    if (encryptedCode) { // 세션스토리지에 암호화된 인증 코드가 존재한다면
+      const decryptedCode = decrypt(encryptedCode); // 인증 코드 복호화
+      // TODO: 인증에 성공하면 입력 코드와 입력 확인 버튼을 비활성화 시켜야 함
+      if (decryptedCode === code) { // 입력한 코드와 복호화된 코드가 같다면
+        sessionStorage.removeItem('email_auth_code');
+        setValue('school_auth', true);
+        return alert('인증성공');
+      } else {
+        return alert('인증실패');
+      }
+    } else {
+      return alert('이메일을 입력하고 이메일 인증 버튼을 눌러주신 후 시도해주세요.');
+    }
   }
 
   return (
@@ -36,14 +61,18 @@ export default function SignupForm() {
         <button
           className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1"
           type="button"
+          onClick={() => emailAuthHandler(watch('school_email'))}
           {...register('school_auth', { value: false })}
         >이메일 인증</button>
       </section>
       <section>
-        <SignupInput placeholder="인증번호" register={{ ...register('school_auth_code') }} />
+        {/* TODO: 이메일 인증에 성공하면 input 배경색 변경하고 버튼의 디자인도 바뀌도록 하기 */}
+        <SignupInput className={`${watch('school_auth') ? 'bg-silver' : ''}`} placeholder="인증번호" register={{ ...register('school_auth_code') }} />
         <button
           className="ml-3 border border-blue text-blue text-sm rounded-md px-3 py-1"
           type="button"
+          onClick={() => emailAuthConfirmHandler(watch('school_auth_code'))}
+          disabled={watch('school_auth')}
         >확인</button>
       </section>
       <section className="mt-10">
@@ -69,6 +98,12 @@ export default function SignupForm() {
   );
 }
 
+function decrypt(code: string) {
+  console.log(process.env.NEXT_PUBLIC_AES_SECRET_KEY);
+  const decrypted = cryptoJS.AES.decrypt(code, process.env.NEXT_PUBLIC_AES_SECRET_KEY);
+  return decrypted.toString(cryptoJS.enc.Utf8);
+}
+
 interface FormInputs {
   id: string;
   password: string;
@@ -81,4 +116,9 @@ interface FormInputs {
   school_email: string;
   school_auth: boolean;
   school_auth_code: string;
+}
+
+interface EmailAuthRes {
+  success: boolean;
+  auth_code: string;
 }
--- src/pages/api/auth/email.ts
+++ src/pages/api/auth/email.ts
@@ -9,7 +9,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
   }
 
   try {
-    const body = JSON.parse(req.body);
+    const body = req.body;
 
     if (!body.email) {
       return res.status(400).send("잘못된 바디 정보");
@@ -17,8 +17,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     // Nodemailer transporter 생성
     let authCode = generateRandomCode();
-    console.log(authCode);
-    console.log(encrypt(authCode));
+
     const transporter = nodemailer.createTransport({
       service: 'gmail',
       auth: {
@@ -31,13 +30,13 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
     const mailOptions = {
       from: `${process.env.BUSINESS_NAME} <${process.env.ADMIN_EMAIL}>`,
       to: body.email,
-      subject: '테스트 이메일',
-      text: '안녕하세요, 이것은 테스트 이메일입니다.',
+      subject: '순동 학교 이메일 인증번호입니다.',
+      html: `<div>안녕하세요. 순동에서 이메일 인증번호를 보내드립니다.</div><div>인증번호: ${authCode}</div>`,
     };
 
     // 이메일 전송
     const info = await transporter.sendMail(mailOptions);
-
+    console.log(authCode);
     console.log('이메일 전송 성공:', info.response);
     return res.status(200).json({ success: true, auth_code: encrypt(authCode) });
   } catch (error) {
@@ -50,11 +49,10 @@ function generateRandomCode() {
   const min = 100000;
   const max = 999999;
   const randomCode = Math.floor(Math.random() * (max - min + 1)) + min;
-
   return String(randomCode);
 }
 
 function encrypt(code: string) {
-  const encrypted = cryptoJS.AES.encrypt(code, process.env.AES_SECRET_KEY).toString();
-  return encrypted;
+  const encrypted = cryptoJS.AES.encrypt(code, process.env.NEXT_PUBLIC_AES_SECRET_KEY);
+  return encrypted.toString();
 }
