04e02d5257b4fc649fea75a9f2626ceea7addfac
TaeWooKim-SCH <zop1234@hanmail.net>
2024-01-24 16:47:37 +0900

    feat: 마이 페이지 레이아웃 구현

--- src/app/_components/my/UserInfo.tsx
+++ src/app/_components/my/UserInfo.tsx
@@ -6,9 +6,13 @@ export default async function UserInfo() {
   const session = await getServerSession(authOptions);
   console.log(session);
   return (
-    <section>
+    <section className="flex justify-center items-center px-24 py-5 border border-blue rounded-md">
       <div>
-        <CgProfile className="sm:size-[40px]" color="#26539C" size="30" />
+        <CgProfile className="sm:size-[50px]" color="#26539C" size="40" />
+      </div>
+      <div className="ml-5">
+        <div className="font-bold">{session?.user.name}</div>
+        <div className="text-sm text-silver">{session?.user.id}</div>
       </div>
     </section>
   );
