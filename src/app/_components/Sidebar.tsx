474839739b3bc60a128ee3dbec83f2e603100416
TaeWooKim-SCH <zop1234@hanmail.net>
2024-02-07 16:26:44 +0900

    fix: 네비게이션바 디자인 수정

--- src/app/_components/Sidebar.tsx
+++ src/app/_components/Sidebar.tsx
@@ -2,7 +2,6 @@
 
 import Link from "next/link";
 import { useState } from "react";
-import { IoClose, IoMenu } from "react-icons/io5";
 
 export default function SideBar() {
   const headerItems = ["모든 동아리", "공연예술", "종교", "봉사", "교양학술", "체육", "전시창작", "준동아리"];
@@ -14,14 +13,16 @@ export default function SideBar() {
 
   return (
     <section className="xl:hidden">
-      <div className="ml-2 cursor-pointer sm:ml-5" onClick={sideBarHandler}>
-        <IoMenu className="sm:size-[40px]" size="30" color="#26539C" />
+      <div className="ml-2 relative w-[30px] h-[30px] flex flex-col justify-evenly cursor-pointer z-50 sm:ml-5" onClick={sideBarHandler}>
+        <div
+          className={`absolute transition-all w-[30px] h-[4px] bg-blue rounded-md ${isSideBar ? 'top-1/2 -translate-y-1/2 rotate-45 bg-white' : 'top-2'}`}
+          ></div>
+        <div
+          className={`absolute transition-all w-[30px] h-[4px] bg-blue rounded-md ${isSideBar ? 'top-1/2 -translate-y-1/2 -rotate-45 bg-white' : 'top-4'}`}
+          ></div>
       </div>
-      <div className={`absolute top-0 right-0 duration-500 h-full bg-blue ${isSideBar ? "translate-x-0" : "translate-x-[100%]"}`}>
-        <div className="text-end mt-3 mr-3 mb-5">
-          <IoClose className="inline-block cursor-pointer sm:size-[40px]" onClick={sideBarHandler} fill="white" size="30" />
-        </div>
-        <div className="px-20 flex flex-col items-center text-white sm:text-xl">
+      <div className={`absolute top-0 right-0 duration-300 w-full h-[100vh] bg-blue z-40 ${isSideBar ? "translate-y-0" : "-translate-y-[100%]"}`}>
+        <div className="px-20 py-24 flex flex-col items-center text-white sm:text-xl">
           {headerItems.map((item) => (
             <Link className="mb-5" href={`/clubs?category=${item}`} key={item}>{item}</Link>
           ))}
