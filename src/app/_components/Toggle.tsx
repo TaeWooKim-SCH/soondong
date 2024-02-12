3c9e1550708d02a05806dcc37b60798685829959
TaeWooKim-SCH <zop1234@hanmail.net>
2024-02-12 16:14:32 +0900

    feat: 토글 컴포넌트 분리

--- src/app/_components/Toggle.tsx
+++ src/app/_components/Toggle.tsx
@@ -1,53 +1,22 @@
 'use client'
 
-import { useToggle } from "../_modules/store";
-
-export default function Toggle({ toggleName }: PropsType) {
-  const {
-    recruiting,
-    joined,
-    updateRecuiting,
-    updateJoined
-  } = useToggle();
-
-  const toggleClickHandler = () => {
-    toggleName === 'recruiting' ? updateRecuiting() : updateJoined();
-  };
-
-  switch (toggleName) {
-    case 'recruiting':
-      return (
-        <div
-          className={`relative w-[45px] h-[20px] sm:w-[55px] sm:h-[25px] rounded-full cursor-pointer ${
-            recruiting ? 'bg-blue' : 'bg-silver'
-          }`}
-          onClick={toggleClickHandler}
-        >
-          <div
-          className={`absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] sm:w-[22px] sm:h-[22px] bg-white rounded-full transition-all ${
-            recruiting ? 'right-[0.1rem]' : 'right-[1.6rem] sm:right-[1.95rem]'
-          }`}
-          ></div>
-        </div>
-      );
-    case 'joined':
-      return (
-        <div
-          className={`relative w-[45px] h-[20px] sm:w-[55px] sm:h-[25px] rounded-full cursor-pointer ${
-            joined ? 'bg-blue' : 'bg-silver'
-          }`}
-          onClick={toggleClickHandler}
-        >
-          <div
-          className={`absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] sm:w-[22px] sm:h-[22px] bg-white rounded-full transition-all ${
-            joined ? 'right-[0.1rem]' : 'right-[1.6rem] sm:right-[1.95rem]'
-          }`}
-          ></div>
-        </div>
-      );
-  }
+export default function Toggle({ state, onClick }: PropsType) {
+  return (
+    <button
+      className={`relative w-[45px] h-[20px] sm:w-[55px] sm:h-[25px] rounded-full cursor-pointer ${
+        state ? 'bg-blue' : 'bg-silver'
+      }`}
+      onClick={onClick}
+    >
+      <div
+      className={`absolute top-1/2 -translate-y-1/2 w-[18px] h-[18px] sm:w-[22px] sm:h-[22px] bg-white rounded-full transition-all ${
+        state ? 'right-[0.1rem]' : 'right-[1.6rem] sm:right-[1.95rem]'
+      }`}
+      ></div>
+    </button>
+  );
 }
 
-interface PropsType {
-  toggleName: 'recruiting' | 'joined';
+interface PropsType extends React.ComponentProps<'button'> {
+  state: boolean;
 }
