4d2760a9dfff295b0bed744657a12f9feecfb635
TaeWooKim-SCH <zop1234@hanmail.net>
2024-02-25 15:37:02 +0900

    feat: 찜 목록 반응형

--- src/app/my/likes/page.tsx
+++ src/app/my/likes/page.tsx
@@ -3,8 +3,8 @@ import Layout from "@/app/_components/layouts/Layout";
 export default function MyLikes() {
   return (
     <Layout className="flex flex-col justify-center items-center">
-      <div className="text-2xl font-bold text-blue mb-5">좋아요 목록 페이지는 구현 예정에 있습니다.</div>
-      <div className="text-2xl font-bold text-blue">빠른 시일 내로 구현하도록 하겠습니다.</div>
+      <div className="font-bold text-blue mb-5 sm:text-2xl">좋아요 목록 페이지는 구현 예정에 있습니다.</div>
+      <div className="font-bold text-blue sm:text-2xl">빠른 시일 내로 구현하도록 하겠습니다.</div>
     </Layout>
   );
 }
