38a5088e027d7d57db18f8b00a8bea9c73ac75cc
TaeWooKim-SCH <zop1234@hanmail.net>
2024-02-27 14:18:56 +0900

    feat: 동아리 가입 질문 프론트 구현

--- src/app/my/clubs/add/_components/ClubAddForm.tsx
+++ src/app/my/clubs/add/_components/ClubAddForm.tsx
@@ -11,6 +11,7 @@ export default function ClubAddForm() {
   const router = useRouter();
   const clubsCategory = ['공연예술', '종교', '봉사', '교양학술', '체육', '전시창작', '준동아리'];
   const [isPeriod, setIsPeriod] = useState<boolean>(false);
+  const [question, setQuestion] = useState<string>('');
   const {
     register,
     handleSubmit,
@@ -23,6 +24,7 @@ export default function ClubAddForm() {
       club_category: '카테고리 선택',
       club_description: '',
       club_post: '',
+      club_join_questions: [],
       club_recruit_period: '모집 방식 선택',
       period_start: '',
       period_end: ''
@@ -45,6 +47,30 @@ export default function ClubAddForm() {
       }
     }
   });
+
+  // 질문 입력 체인지 핸들러
+  const questionChangeHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setQuestion(e.target.value);
+  }
+
+  // 질문 입력이 끝난 후 추가 버튼 클릭 핸들러
+  const questionAddClickHandler = () => {
+    if (!question) {
+      return alert('질문 내용을 입력해주세요.');
+    }
+    if (watch('club_join_questions').length >= 3) {
+      return alert('질문은 최대 3개까지만 등록할 수 있습니다.');
+    }
+    const result = [...watch('club_join_questions')];
+    result.push(question);
+    setValue('club_join_questions', result);
+    setQuestion('');
+  }
+
+  const questionCancleClickHandler = (idx: number) => {
+    const result = watch('club_join_questions').filter((el, elIdx) => idx !== elIdx);
+    setValue('club_join_questions', result);
+  }
 
   // 동아리 개설 신청 핸들러
   const onSubmit: SubmitHandler<FormInputs> = throttle(async (data) => {
@@ -71,7 +97,6 @@ export default function ClubAddForm() {
 
       const res = await fetch('/api/clubs/add-img', {
         method: 'POST',
-        // headers: { "Content-Type": "multipart/form-data" },
         body: form
       });
 
@@ -89,6 +114,7 @@ export default function ClubAddForm() {
           club_category: data.club_category,
           club_description: data.club_description,
           club_post: data.club_post,
+          club_join_questions: data.club_join_questions,
           club_recruit_period: data.club_recruit_period,
           period_start: data.period_start,
           period_end: data.period_end,
@@ -116,7 +142,7 @@ export default function ClubAddForm() {
   });
 
   return (
-    <form className="grid grid-cols-1 gap-5" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
+    <form className="grid grid-cols-1 gap-7" onSubmit={handleSubmit(onSubmit)} autoComplete="off">
       <section className="grid md:gap-10 md:grid-cols-2">
         <article className="mb-5 md:mb-0">
           <div className="text-lg text-blue font-bold mb-3">동아리 이름</div>
@@ -165,7 +191,7 @@ export default function ClubAddForm() {
           {...register('club_img')}
         />
       </section>
-      <section className="mb-10">
+      <section>
         <div className="text-lg text-blue font-bold mb-3">동아리 모집기간</div>
         <article className="sm:flex items-center">
           <section>
@@ -196,7 +222,36 @@ export default function ClubAddForm() {
           )}
         </article>
       </section>
-      <button className="bg-blue py-2 w-[100px] mx-auto rounded-md text-white text-bold">개설신청</button>
+      <section className="mb-10">
+        <div className="text-lg text-blue font-bold mb-3">동아리 가입질문 ( 최대 3개 ) - 선택사항</div>
+        <article className="sm:grid sm:grid-cols-5 sm:gap-5">
+          <input
+            className="px-3 py-2 rounded-md outline-none w-full border border-silver sm:col-span-4"
+            type="text"
+            value={question}
+            onChange={questionChangeHandler}
+            placeholder="동아리 가입신청 시 받을 질문을 입력해주세요"
+          />
+          <button
+            className="bg-blue py-2 px-7 mt-3 mx-auto rounded-md text-white text-sm sm:text-base sm:mt-0"
+            type="button"
+            onClick={questionAddClickHandler}
+          >추가</button>
+        </article>
+        <article>
+          {watch('club_join_questions').length ? watch('club_join_questions').map((question, idx) => (
+            <div className="flex items-center mt-3" key={idx}>
+              <div className="font-bold text-blue text-sm sm:text-base">Q{idx + 1}. {question}</div>
+              <button
+                className="px-3 py-1 ml-3 border-[1.5px] border-red rounded-md text-xs sm:text-sm"
+                type="button"
+                onClick={() => questionCancleClickHandler(idx)}
+              >취소</button>
+            </div>
+          )) : null}
+        </article>
+      </section>
+      <button className="bg-blue py-2 w-[100px] mx-auto rounded-md text-white">개설신청</button>
       { isSubmitting && <LoadingUI /> }
     </form>
   );
@@ -207,6 +262,7 @@ interface FormInputs {
   club_category: '카테고리 선택' | '공연예술' | '종교' | '봉사' | '교양학술' | '체육' | '전시창작' | '준동아리';
   club_description: string;
   club_post: string;
+  club_join_questions: string[];
   club_img: File;
   club_img_url: string;
   club_recruit_period: string;
