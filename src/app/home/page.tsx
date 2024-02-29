1b38d16811c5a246ca601d095a6fc5116463036c
TaeWooKim-SCH <zop1234@hanmail.net>
2024-02-29 16:53:23 +0900

    feat: 캐러셀 구현

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,6 +1,7 @@
 import Layout from "../_components/layouts/Layout";
 import PostCard from "../_components/PostCard";
 import Title from "../_components/Title";
+import Carousel from "./_components/Carousel";
 
 async function getData() {
   const res = await fetch(`${process.env.NEXT_PUBLIC_DOMAIN}/api/home`, { cache: 'no-store' });
@@ -16,8 +17,9 @@ export default async function Home() {
   const clubsData: ClubsData[] = await getData();
 
   return (
-    <Layout>
-      <section className="mb-10">
+    <Layout className="px-0 py-0 sm:px-0 md:px-0 xl:px-0">
+      <Carousel clubsData={clubsData} />
+      <section className="mb-10 px-10 py-10 sm:px-16 md:px-28 xl:px-44">
         <Title className="mb-5">현재 모집중인 동아리</Title>
         <div className="grid grid-cols-1 gap-6 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 md:gap-10">
           {clubsData.map((clubInfo) => (
--- src/app/my/clubs/add/page.tsx
+++ src/app/my/clubs/add/page.tsx
@@ -2,6 +2,7 @@ import Layout from "@/app/_components/layouts/Layout";
 import ClubAddForm from "@/app/my/clubs/add/_components/ClubAddForm";
 
 export default function AddClub() {
+  // TODO: 동아리 포스터가 없어도 개설 가능하도록 구현. 이미지가 없을 땐 순동 기본이미지로 보여주기
   return (
     <Layout className="flex flex-col items-center">
       <section className="flex flex-col items-center mb-10">
