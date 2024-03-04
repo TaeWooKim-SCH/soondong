86ef2359654ef36580c8efc96ff01adc3bdab512
TaeWooKim-SCH <zop1234@hanmail.net>
2024-03-04 14:05:35 +0900

    feat: 관리자 로그인 페이지 레이아웃 구현

---
+++ src/app/admin/page.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+
+import Layout from "../_components/layouts/Layout";
+import AdminLoginForm from "./_components/AdminLoginForm";
+
+export default function AdminLogin() {
+  return (
+    <Layout className="flex justify-center items-center">
+      <section className="mx-auto mb-20 flex flex-col justify-center items-center rounded-lg sm:py-32 sm:border sm:border-blue sm:px-20">
+        {/* <Image className="w-[250px] mb-16" src="/Logo.svg" alt="로고" width={500} height={0} priority={true} /> */}
+        <div className="text-2xl text-blue font-bold mb-16">관리자 로그인</div>
+        <AdminLoginForm />
+      </section>
+    </Layout>
+  );
+}
