35680b4f38c9e26b291868388ab376bca5167536
TaeWooKim-SCH <zop1234@hanmail.net>
2024-03-06 14:00:43 +0900

    feat: 관리자 로그인 후 리다이렉트

--- src/app/admin/_components/AdminLoginForm.tsx
+++ src/app/admin/_components/AdminLoginForm.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+import { useRouter } from "next/navigation";
 import { SubmitHandler, useForm } from "react-hook-form";
 import { throttle } from "lodash";
 import { FaUser } from "react-icons/fa";
@@ -9,6 +10,7 @@ import { encrypt } from "@/utils/modules";
 import LoadingUI from "../../_components/LoadingUI";
 
 export default function AdminLoginForm() {
+  const router = useRouter();
   const { register, handleSubmit, formState: { isSubmitting } } = useForm<FormInputs>({
     defaultValues: { id: '', password: ''}
   });
@@ -31,7 +33,7 @@ export default function AdminLoginForm() {
       if (!res.ok) {
         return alert('아이디 또는 비밀번호가 일치하지 않습니다.');
       } else {
-        return alert('로그인 성공');
+        router.push('/admin/home');
       }
     } catch (err) {
       console.error('로그인 실패', err);
---
+++ src/app/admin/home/page.tsx
@@ -0,0 +1,9 @@
+import Layout from "@/app/_components/layouts/Layout";
+
+export default function AdminHome() {
+  return (
+    <Layout>
+      <div>메인</div>
+    </Layout>
+  );
+}
