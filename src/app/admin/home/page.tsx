4a3ef6f713b698a85751a900888ecfc1db2c35ba
TaeWooKim-SCH <zop1234@hanmail.net>
2024-03-07 16:25:17 +0900

    fix: 스타일 수정

--- src/app/admin/home/page.tsx
+++ src/app/admin/home/page.tsx
@@ -3,7 +3,7 @@ import Layout from "@/app/_components/layouts/Layout";
 export default function AdminHome() {
   return (
     <Layout>
-      <div>메인</div>
+      <div className="text-blue font-bold text-2xl">동아리 개설 신청 내역</div>
     </Layout>
   );
 }
